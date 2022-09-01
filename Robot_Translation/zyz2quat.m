89c25c5f28ca70209147391c5f2f54bf284c0646
tan-shizhong <948766026@qq.com>
2022-09-01 10:46:14 +0800

    欧拉角转四元数

---
+++ Robot_Translation/zyz2quat.m
@@ -0,0 +1,15 @@
+function q = zyz2quat(oula_angle)
+% q = zyz2quat(oula_angle)
+% 将zyz欧拉角转化为四元数，可以同时输入多个欧拉角，为n*3格式
+
+angles = oula_angle;
+
+cang = cos( angles/2 );
+sang = sin( angles/2 );
+
+q = [ cang(:,1).*cang(:,2).*cang(:,3) - sang(:,1).*cang(:,2).*sang(:,3), ...
+            cang(:,1).*sang(:,2).*sang(:,3) - sang(:,1).*sang(:,2).*cang(:,3), ...
+            cang(:,1).*sang(:,2).*cang(:,3) + sang(:,1).*sang(:,2).*sang(:,3), ...
+            sang(:,1).*cang(:,2).*cang(:,3) + cang(:,1).*cang(:,2).*sang(:,3)];
+
+end
