1168d76a23997129447003de6e50cce7738e42be
tan-shizhong <948766026@qq.com>
2022-09-01 15:23:35 +0800

    ZYZ修改

--- Robot_Translation/matrix2zyz.m
+++ Robot_Translation/matrix2zyz.m
@@ -5,18 +5,26 @@ oula_zyz(1) = T(1,4);
 oula_zyz(2) = T(2,4);
 oula_zyz(3) = T(3,4);
 
-R_rot = T(1:3,1:3);
+Zaxis=T(1:3,3);
 
-if R_rot(1,3) ~= 0 && R_rot(2,3) ~= 0 
-    beta=atan2(sqrt(R_rot(3,1)*R_rot(3,1)+R_rot(3,2)*R_rot(3,2)),R_rot(3,3));
-    gamma = atan2(R_rot(3,2)/sin(beta),-R_rot(3,1)/sin(beta));
-    alpha = atan2(R_rot(2,3)/sin(beta),R_rot(1,3)/sin(beta));
+if abs(Zaxis(1))<0.0001 && abs(Zaxis(2))<0.0001
+    if Zaxis(3)>0.9
+        %disp('---------------------奇异，beta=0---------------------');
+        beta=0;
+        alpha=0;
+        gamma=atan2(-T(1,2),T(1,1));
+    else 
+        %disp('---------------------奇异，beta=pi---------------------');
+        beta=pi;
+        alpha=0;
+        gamma=atan2(T(1,2),-T(1,1));
+    end
 else
-    disp('---------------------奇异，beta=0---------------------');
-    beta=0;
-    alpha=0;
-    gamma=atan2(-R_rot(1,2),R_rot(1,1));
+    beta=atan2(sqrt(T(3,1)*T(3,1)+T(3,2)*T(3,2)),Zaxis(3));
+    alpha=atan2(Zaxis(2),Zaxis(1));
+    gamma=atan2(T(3,2),-T(3,1));
 end
+
 oula_zyz(4)=alpha;
 oula_zyz(5)=beta;
 oula_zyz(6)=gamma;
--- Robot_Translation/test.m
+++ Robot_Translation/test.m
@@ -8,10 +8,10 @@ oula = [x y z a b c];
 
 matrix = zyz2matrix(oula)
 
-oula_1 = [100 200 300 0.7 1.4 3];
+oula_1 = [100 200 300 2.4 -1.4 3.0];
 
 matrix_1 = xyz2matrix(oula_1)
 
 oula_zyz = matrix2xyz(matrix_1)
 
-oula_zyz_matlab = tform2eul(matrix_1,'XYZ')
+oula_zyz_matlab = tform2eul(matrix_1,'ZYZ')
